310551e0a1d1e8fc0f618dbee8a0f725d5c67842
LavishLitLogos <lavishlitlogos@gmail.com>
2026-06-27 03:34:57 -0400

    Hardwire Supabase client for production

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://jdkpdvaexwbmnoggkleh.supabase.co';
+
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impka3BkdmFleHdibW5vZ2drbGVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0OTA5MzYsImV4cCI6MjA5NzA2NjkzNn0.lDKDkXIsZL6j_TOA6xOM7h2m1REihqzvU6YgDwhdhro';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
